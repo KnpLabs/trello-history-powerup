1c7fcd6e6639eefbc601e08530eb31e262852ff7
Joris Langlois <joris.langlois@knplabs.com>
2019-06-07 17:31:00 +0200

    Directly use powerup api client

--- public/js/history.js
+++ public/js/history.js
@@ -1,22 +1,27 @@
-// Connect to trello API
-const authenticationSuccess = () => console.log('Successful authentication')
-const authenticationFailure = () => console.log('Failed authentication')
+// // Connect to trello API
+// const authenticationSuccess = () => console.log('Successful authentication')
+// const authenticationFailure = () => console.log('Failed authentication')
+//
+// window.Trello.authorize({
+// 		type: 'popup',
+// 		name: 'KNP Trello Extension',
+// 		scope: {
+// 	    read: 'true',
+// 	    write: 'true'
+// 		},
+// 		expiration: 'never',
+// 		success: authenticationSuccess,
+// 		error: authenticationFailure,
+// })
+//
+// // Get card history
+// const success = data => console.warn(data)
+// const error = data => console.warn(data)
+// // TODO: dynamically get card id
+// const cardId = 'CNcJLRUB';
+//
+// window.Trello.get(`/cards/${cardId}`, success, error);
 
-window.Trello.authorize({
-		type: 'popup',
-		name: 'KNP Trello Extension',
-		scope: {
-	    read: 'true',
-	    write: 'true'
-		},
-		expiration: 'never',
-		success: authenticationSuccess,
-		error: authenticationFailure,
-})
+const t = window.TrelloPowerUp.iframe();
 
-// Get card history
-const success = data => console.warn(data)
-const error = data => console.warn(data)
-const cardId = window.location.pathname.split('/')[2];
-console.warn(cardId);
-window.Trello.get(`/cards/${cardId}`, success, error);
+t.get('card').then(a => console.warn(a))
--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,44 @@
-var Promise = TrelloPowerUp.Promise;
+const Promise = TrelloPowerUp.Promise
 
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
+const BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
+const API_KEY = '3fdd940c3f6ea9c4f8ed0817a71b1a4c'
 
-TrelloPowerUp.initialize({
+window.TrelloPowerUp.initialize({
 	'card-buttons': (t, options) =>
-		t.set("member", "shared", "hello", "world")
-		.then(() => [{
-	      icon: BLACK_ROCKET_ICON,
-	 		  text: 'History',
-	      callback: t => t.popup({
-					title: "History",
-	        url: 'history.html',
-	      }),
-		}])
-});
+		// t.set("member", "shared", "hello", "world")
+		t.getRestApi()
+			.getToken()
+			.then(token => getCardHistory(token))
+			.then(history => {
+				console.warn(history);
+
+				return [{
+		      icon: BLACK_ROCKET_ICON,
+		 		  text: 'History',
+		      callback: t => t.popup({
+						title: "History",
+		        url: 'history.html',
+		      }),
+				}];
+		})
+}, {
+	appKey: API_KEY,
+	appName: 'KNP Trello Extension',
+})
+
+// getCardHistory :: String -> Promise
+const getCardHistory = token => new Promise((resolve, reject) => {
+	const xhr = new XMLHttpRequest()
+	// TODO: make this dynamic
+	const cardId = 'CNcJLRUB'
+
+	xhr.open('GET', `https://api.trello.com/1/cards/${cardId}/actions?filter=updateCard:desc&key=${API_KEY}&token=${token}`)
+
+	xhr.onload = () => (xhr.status >= 200 && xhr.status < 300)
+				? resolve(JSON.parse(xhr.response))
+				: reject(xhr.statusText)
+
+	xhr.onerror = () => reject(xhr.statusText)
+
+	xhr.send()
+})
