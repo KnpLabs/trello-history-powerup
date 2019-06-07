6d77ada784756b0b6c344c94b7f8a478412adcde
Joris Langlois <joris.langlois@knplabs.com>
2019-06-07 16:04:25 +0200

    changed auth method

--- public/js/history.js
+++ public/js/history.js
@@ -1,14 +1,4 @@
-var creationSuccess = function (data) {
-  console.log('Card created successfully.');
-  console.log(JSON.stringify(data, null, 2));
-};
+const success = data => console.warn(data)
+const errror = data => console.warn(data)
 
-var newCard = {
-  name: 'New Test Card',
-  desc: 'This is the description of our new card.',
-  // Place this card at the top of our list
-  idList: "5cfa2939e3877865b4f93be0",
-  pos: 'top'
-};
-
-window.Trello.post('/cards/', newCard, creationSuccess);
+Trello.get('/cards/CNcJLRUB', success, error);
