ddd2224c7b3bbd13a1f571b9a437695cae43004c
Joris Langlois <joris.langlois@knplabs.com>
2019-06-07 16:14:10 +0200

    moved trello authentification

--- public/js/history.js
+++ public/js/history.js
@@ -1,4 +1,20 @@
 const success = data => console.warn(data)
 const error = data => console.warn(data)
 
+// Connect to trello API
+const authenticationSuccess = () => console.log('Successful authentication')
+const authenticationFailure = () => console.log('Failed authentication')
+
+window.Trello.authorize({
+		type: 'popup',
+		name: 'KNP Trello Extension',
+		scope: {
+	    read: 'true',
+	    write: 'true'
+		},
+		expiration: 'never',
+		success: authenticationSuccess,
+		error: authenticationFailure,
+})
+
 window.Trello.get('/cards/CNcJLRUB', success, error);
