2372e89af4d2caad39581e1003df4412be4c5079
Emma <emmanuelle.collet@knplabs.com>
2019-06-13 11:40:15 +0200

    Add history feature

--- public/js/history.js
+++ public/js/history.js
@@ -8,18 +8,16 @@ const renderCard = card => {
   const historyElement = document.createElement('article')
 
   historyElement.innerHTML = `
-    <article>
-        <div>
-            <img class="member-avatar" src="${card['memberCreator']['avatarUrl']}/30.png" srcset="${card['memberCreator']['avatarUrl']}/30.png 1x" title="${card['memberCreator']['fullName']} (${card['memberCreator']['username']})">
-            <span>${card['memberCreator']['fullName']}</span>
-            <br>
-            <span>${formatDate(card['date'])}</span>
-        </div>
-        <div>
-            <h4>${card['data']['card']['name']}</h4>
-            <div>${card['data']['card']['desc']}</div>
-        </div>
-    </article>
+    <div class="header">
+      <img class="member-avatar" src="${card['memberCreator']['avatarUrl']}/30.png" srcset="${card['memberCreator']['avatarUrl']}/30.png 1x" title="${card['memberCreator']['fullName']} (${card['memberCreator']['username']})">
+      <span class="member-name">${card['memberCreator']['fullName']}</span>
+      <br>
+      <span class="date">${formatDate(card['date'])}</span>
+    </div>
+    <div class="content">
+      <h4 class="title">${card['data']['card']['name']}</h4>
+      <div class="desc">${card['data']['card']['desc']}</div>
+    </div>
   `
 
   document.getElementById('history').appendChild(historyElement)
