0321d16fe2368203246b718ee29e4d0d3803ab56
Emma <emmanuelle.collet@knplabs.com>
2019-06-14 15:22:40 +0200

    Add translations

--- public/js/index.js
+++ public/js/index.js
@@ -2,34 +2,49 @@ const Promise = window.TrelloPowerUp.Promise
 
 const BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
 
-const askAuthorization = t => ({
-  title: 'Authorize to continue',
-  icon: BLACK_ROCKET_ICON,
-  content: {
-    type: 'iframe',
-    url: t.signUrl('./authorize.html'),
-    height: 110
-  }
-})
+const local = navigator.language || navigator.userLanguage || 'en';
+const translationsFile = `translations/${local.substring(0,2)}.json`;
+const allowedLocales = ['en', 'fr'];
 
-const renderHistory = t => ({
-  title: 'History',
-  icon: BLACK_ROCKET_ICON,
-  content: {
-    type: 'iframe',
-    url: t.signUrl('./history.html'),
-    height: 250
-  }
-})
+const getTranslations = () => allowedLocales.includes(local.substring(0,2))
+  ? fetch(translationsFile, {method: 'GET'}).then(response => response.json())
+  : fetch(`translations/en.json`, {method: 'GET'}).then(response => response.json())
+
+const askAuthorization = t => t.get('organization', 'shared', 'translations')
+  .then(translations => ({
+    title: translations.authorize_to_continue,
+    icon: BLACK_ROCKET_ICON,
+    content: {
+      type: 'iframe',
+      url: t.signUrl('./authorize.html'),
+      height: 110
+    }
+  })
+)
+
+const renderHistory = t => t.get('organization', 'shared', 'translations')
+  .then(translations => ({
+    title: translations.history,
+    icon: BLACK_ROCKET_ICON,
+    content: {
+      type: 'iframe',
+      url: t.signUrl('./history.html'),
+      height: 250
+    }
+  })
+)
 
 window.TrelloPowerUp.initialize({
-  'card-back-section': (t, options) => t.getRestApi()
-    .isAuthorized()
-    .then(isAuthorized => isAuthorized
-      ? renderHistory(t)
-      : askAuthorization(t)
-    ),
+  'card-back-section': (t, options) =>  getTranslations()
+    .then(translations => t.set('organization', 'shared', 'translations', translations))
+    .then(() => t.getRestApi()
+      .isAuthorized()
+      .then(isAuthorized => isAuthorized
+        ? renderHistory(t)
+        : askAuthorization(t)
+      ),
+    )
 }, {
   appKey: '3fdd940c3f6ea9c4f8ed0817a71b1a4c',
-  appName: 'KNP Trello Extension',
+  appName: 'KNP Trello History',
 })
